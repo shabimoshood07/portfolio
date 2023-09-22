5166cfbdc9fc1dec4d91f708919cff679d314daa
unknown <shabimoshood07@gmail.com>
2023-09-22 09:57:47 +0100

    .

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Contact = () => {
   return (
     <div
@@ -5,7 +7,13 @@ const Contact = () => {
       className="bg-gray-300  flex justify-center items-center py-20"
     >
       <div className="w-[98%] max-w-4xl mx-auto px-3">
-        <h1 className="text-[25px] font-bold section-heading relative inline-block mb-3">Contact</h1>
+        <h1 className="text-[25px] font-bold section-heading relative inline-block mb-3">
+          Contact
+        </h1>
+        <p className="text-[20px] font-medium">Ready to join forces or bring me on board?</p>
+        <Link type="email" href="mailto:shabimoshood07@gmail.com" className="text-[25px] font-bold hover:text-red-500 duration-300">
+          Let's connect via email! 🚀📩
+        </Link>
       </div>
     </div>
   );
